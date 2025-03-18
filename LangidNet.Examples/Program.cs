16e5941748af5f913b5401294075aea16bfdbadf
manyeyes <zx.520.521@126.com>
2025-03-18 12:08:31 +0800

    update namespace

---
+++ LangidNet.Examples/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://github.com/manyeyes for more information
+// Copyright (c)  2023 by manyeyes
+using LangidNet;
+
+namespace ManyVitsTts.Examples
+{
+    internal static partial class Program
+    {
+        public static string applicationBase = AppDomain.CurrentDomain.BaseDirectory;
+        [STAThread]
+        private static void Main()
+        {
+            TestLangid();
+        }
+        public static void TestLangid()
+        {
+            Console.WriteLine("Please enter text:");
+            while (true)
+            {
+                string text = Console.ReadLine();
+                string ldpyFilePath = "";//applicationBase + "/" + "ldpy.model";
+                LangidNet.LangidRecognizer langidRecognizer = new LangidNet.LangidRecognizer(ldpyFilePath: ldpyFilePath);
+                LangidNet.TextStream stream = langidRecognizer.CreateTextStream();
+                stream.AddText(text);
+                LangidNet.Model.LangidRecognizerResultEntity langidRecognizerResultEntity = new LangidNet.Model.LangidRecognizerResultEntity();
+                langidRecognizerResultEntity = langidRecognizer.GetResult(stream);
+                Console.WriteLine(langidRecognizerResultEntity.Language);
+            }
+        }
+    }
+}
+
+
